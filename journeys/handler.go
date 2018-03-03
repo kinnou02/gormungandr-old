d55cb0701bbbaba746319201fecd25c5a1632948
Alexandre Jacquin <kinou@rilynett.com>
2018-03-03 18:03:28 +0100

    use Kraken from gonavitia

--- journeys/handler.go
+++ journeys/handler.go
@@ -1,84 +1,58 @@
 package journeys
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "github.com/kinnou02/gonavitia/serializer"
-    zmq "github.com/pebbe/zmq2"
-    "github.com/kinnou02/pbnavitia"
-    "github.com/golang/protobuf/proto"
-    "time"
-    "errors"
-    log "github.com/sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+	"github.com/kinnou02/gonavitia"
+	"github.com/kinnou02/gonavitia/serializer"
+	"github.com/kinnou02/pbnavitia"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"time"
 )
 
-func JourneysHandler(kraken string, timeout time.Duration) gin.HandlerFunc {
-    fn := func(c *gin.Context) {
-        req := BuildRequest(c.Query("from"), c.Query("to"))
-        resp, err := CallKraken(kraken, req, timeout)
-        if err != nil {
-            log.Error("failure to call kraken %s", err)
-            c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-            return
-        }
-        r := serializer.NewJourneysReponse(resp)
-    //    fmt.Println(resp)
-        c.JSON(http.StatusOK, r)
-    }
-    return gin.HandlerFunc(fn)
+func JourneysHandler(kraken *gonavitia.Kraken) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		req := BuildRequest(c.Query("from"), c.Query("to"))
+		resp, err := kraken.Call(req)
+		if err != nil {
+			log.Error("failure to call kraken %s", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		r := serializer.NewJourneysReponse(resp)
+		//    fmt.Println(resp)
+		c.JSON(http.StatusOK, r)
+	}
+	return gin.HandlerFunc(fn)
 }
 
-func CallKraken(kraken string, request *pbnavitia.Request,
-                timeout time.Duration) (*pbnavitia.Response, error){
-    requester, _ := zmq.NewSocket(zmq.REQ)
-    requester.Connect(kraken)
-    defer requester.Close()
-    data, _ := proto.Marshal(request)
-    requester.Send(string(data), 0)
-    poller := zmq.NewPoller()
-    poller.Add(requester, zmq.POLLIN)
-    p, err := poller.Poll(timeout)
-    if err != nil {
-        return nil, err
-    }
-    if len(p) < 1 {
-        return nil, errors.New("fucked")
-    }
-    raw_resp, _ := p[0].Socket.Recv(0)
-    resp := &pbnavitia.Response{}
-    _ = proto.Unmarshal([]byte(raw_resp), resp)
-    return resp, nil
-}
+func BuildRequest(from, to string) *pbnavitia.Request {
+	j := &pbnavitia.JourneysRequest{
+		Origin: []*pbnavitia.LocationContext{{
+			Place:          proto.String(from),
+			AccessDuration: proto.Int32(0)},
+		},
+		Destination: []*pbnavitia.LocationContext{{
+			Place:          proto.String(to),
+			AccessDuration: proto.Int32(0)},
+		},
+		Datetimes:              []uint64{uint64(time.Now().Unix())},
+		Clockwise:              proto.Bool(true),
+		MaxDuration:            proto.Int(86400),
+		MaxTransfers:           proto.Int(10),
+		Wheelchair:             proto.Bool(false),
+		ShowCodes:              proto.Bool(false),
+		RealtimeLevel:          pbnavitia.RTLevel_BASE_SCHEDULE.Enum(),
+		MaxExtraSecondPass:     proto.Int(0),
+		WalkingTransferPenalty: proto.Int(120),
+		DirectPathDuration:     proto.Int(30 * 60),
+		BikeInPt:               proto.Bool(false),
+	}
+	req := &pbnavitia.Request{
+		RequestedApi: pbnavitia.API_PLANNER.Enum(),
+		Journeys:     j,
+	}
 
-
-
-func BuildRequest(from, to string) *pbnavitia.Request{
-    j := &pbnavitia.JourneysRequest{
-        Origin: []*pbnavitia.LocationContext{{
-            Place: proto.String(from),
-            AccessDuration: proto.Int32(0)},
-        },
-        Destination: []*pbnavitia.LocationContext{{
-            Place: proto.String(to),
-            AccessDuration: proto.Int32(0)},
-        },
-        Datetimes: []uint64{uint64(time.Now().Unix())},
-        Clockwise: proto.Bool(true),
-        MaxDuration: proto.Int(86400),
-        MaxTransfers: proto.Int(10),
-        Wheelchair: proto.Bool(false),
-        ShowCodes: proto.Bool(false),
-        RealtimeLevel: pbnavitia.RTLevel_BASE_SCHEDULE.Enum(),
-        MaxExtraSecondPass: proto.Int(0),
-        WalkingTransferPenalty: proto.Int(120),
-        DirectPathDuration: proto.Int(30*60),
-        BikeInPt: proto.Bool(false),
-    }
-    req := &pbnavitia.Request{
-        RequestedApi: pbnavitia.API_PLANNER.Enum(),
-        Journeys: j,
-    }
-
-
-    return req
+	return req
 }
--- main.go
+++ main.go
@@ -1,55 +1,59 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/kinnou02/gormungandr/handlers"
-    "github.com/kinnou02/gormungandr/journeys"
-    "github.com/gin-gonic/contrib/ginrus"
-    "github.com/sirupsen/logrus"
-    "time"
-    "flag"
-    "os"
+	"flag"
+	"github.com/gin-gonic/contrib/ginrus"
+	"github.com/gin-gonic/gin"
+	"github.com/kinnou02/gonavitia"
+	"github.com/kinnou02/gormungandr/handlers"
+	"github.com/kinnou02/gormungandr/journeys"
+	"github.com/sirupsen/logrus"
+	"os"
+	"time"
 )
 
-
 func setupRouter() *gin.Engine {
-    r := gin.New()
-    // Recovery middleware recovers from any panics and writes a 500 if there was one.
-    r.Use(gin.Recovery())
+	r := gin.New()
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
 
-    r.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, false))
+	r.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, false))
 
-    r.GET("/status", handlers.Index)
+	r.GET("/status", handlers.Index)
 
-
-    return r
+	return r
 }
 
 func init_log(logjson bool) {
-  // Log as JSON instead of the default ASCII formatter.
-  if logjson {
-    logrus.SetFormatter(&logrus.JSONFormatter{})
-  }
+	// Log as JSON instead of the default ASCII formatter.
+	if logjson {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
 
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  logrus.SetOutput(os.Stdout)
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logrus.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
-  logrus.SetLevel(logrus.DebugLevel)
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.DebugLevel)
 }
 
 func main() {
-    listen := flag.String("listen", ":8080", "[IP]:PORT to listen")
-    timeout := flag.Duration("timeout", time.Second, "timeour for call to kraken")
-    kraken := flag.String("kraken", "tcp://localhost:30000", "zmq addr for kraken")
-    logjson := flag.Bool("logjson", false, "enable json logging")
-    flag.Parse()
-    init_log(*logjson)
-    logrus.Info("timeout: %s", *timeout)
+	listen := flag.String("listen", ":8080", "[IP]:PORT to listen")
+	timeout := flag.Duration("timeout", time.Second, "timeour for call to kraken")
+	kraken_addr := flag.String("kraken", "tcp://localhost:30000", "zmq addr for kraken")
+	logjson := flag.Bool("logjson", false, "enable json logging")
+	flag.Parse()
+	init_log(*logjson)
 
-    r := setupRouter()
-    r.GET("/journeys", journeys.JourneysHandler(*kraken, *timeout))
-    // Listen and Server in 0.0.0.0:8080
-    r.Run(*listen)
+	kraken := &gonavitia.Kraken{
+		Name:    "default",
+		Timeout: *timeout,
+		Addr:    *kraken_addr,
+	}
+
+	r := setupRouter()
+	r.GET("/journeys", journeys.JourneysHandler(kraken))
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(*listen)
 }
